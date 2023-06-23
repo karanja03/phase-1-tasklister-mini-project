35a9c3a56730ff346e8f06729b18f46f530ec764
karanja03 <wambuik03@gmail.com>
2023-06-23 14:03:28 +0300

    Complete tasklister project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.getElementById('create-task-form')
+  let mainLi=document.getElementById('tasks')
+  form.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    const input=document.querySelector("#new-task-description");
+    const inputValue=input.value
+    if(inputValue){
+      const newList=document.createElement('li');
+    const btn=document.createElement('button')
+    btn.textContent=  'x'
+    btn.addEventListener('click',deleteButton)
+      newList.innerText=`${inputValue} `;
+      newList.append(btn);
+      
+      mainLi.appendChild(newList)
+      input.value='';
+    }
+
+
+   
+})
+})
+function deleteButton(e){
+  e.target.parentNode.remove()
+}
+
+
+
